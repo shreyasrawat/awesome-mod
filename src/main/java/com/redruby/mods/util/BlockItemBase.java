340bcaa063882f4b625775cbfff2fccae7e06461
shreyasrawat <69212971+shreyasrawat@users.noreply.github.com>
2020-12-06 18:00:12 -0800

    Migrated to 1.16.1 and added Ruby Ore

--- src/main/java/com/redruby/mods/util/BlockItemBase.java
+++ src/main/java/com/redruby/mods/util/BlockItemBase.java
@@ -5,7 +5,6 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 
 public class BlockItemBase extends BlockItem {
-
     public BlockItemBase(Block block) { super(block, new Item.Properties().group(RedRubyMain.TAB));
     }
 }
